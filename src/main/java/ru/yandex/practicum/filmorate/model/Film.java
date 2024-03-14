5193c5988703a29b321ae200926340e5bdd8230e
ernokanst <ernokanst@gmail.com>
2024-03-14 14:33:50 +0300

    Исправление whitespace

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -12,7 +12,7 @@ public class Film {
     private int id;
     private String name;
     private String description;
-    @JsonFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
     private int duration;
 
